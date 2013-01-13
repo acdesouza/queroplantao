defa66f2bf65ab961a75368090303d292e82f89b
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2013-01-13 09:52:47 -0200

    Projeto novo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PegaMedico::Application.config.secret_token = 'f6010caad12c1f2be2799e735b21f900eef1226db01b3cb66e5ee293784288d9388bb9075b125c2f8248197989fc49bee36c70089090bb6738923e5d70a87d50'
