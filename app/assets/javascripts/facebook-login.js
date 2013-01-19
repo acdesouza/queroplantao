dea16d06e8c3e52aa72ac79954bf75dc7e8ad0ed
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2013-01-19 19:25:32 -0200

    [WiP] Testar o envio de mensagens para o grupo.

--- app/assets/javascripts/facebook-login.js
+++ app/assets/javascripts/facebook-login.js
@@ -1,10 +1,11 @@
 window.fbAsyncInit = function() {
     FB.init({
         appId      : '492391947466622', // App ID
-    channelUrl : '//queroplantao.com.br', // Channel File
+        channelUrl : '//queroplantao.com.br', // Channel File
         status     : true, // check login status
-    cookie     : true, // enable cookies to allow the server to access the session
-    xfbml      : true  // parse XFBML
+        cookie     : true, // enable cookies to allow the server to access the session
+        xfbml      : true,  // parse XFBML
+        frictionlessRequests : true
     });
     // Additional initialization code here
 };
@@ -30,11 +31,39 @@ function fb_login(){
                     nome = response.name;
                     email = response.email;
                     avatar = "http://graph.facebook.com/"+id_profile+"/picture?type=square";
-                   // autenticaFB(id_profile, nome, avatar, email, access_token, origem);
+                    // autenticaFB(id_profile, nome, avatar, email, access_token, origem);
                 }
             });
         }
     },{scope: 'email,publish_stream, sms'});
 }
 
+var members = []
+function getGroupMembersIds() {
+    if (members.length > 0) {
+        return members;
+    }
+
+    //Grupo, PLANTÕES Whatsapp RJ: 132347300260952
+    //Grupo, Quero Plantao: 319924641441589
+    var members = [];
+    FB.api('319924641441589/members', {limit: 0}, function (response) {
+        var total = response.data.length;
+        for(var i=0; i < total; i++) {
+            var member = response.data[i];
+            members[members.length] = member.id;
+        }
+    });
+
+    return members;
+}
+
+// http://developers.facebook.com/docs/reference/api/group/
+function sendRequestToRecipients(plantaoUrl) {
+    FB.ui({method: 'apprequests',
+        message: 'Novo plantão adicionado.',
+        redirect_uri: plantaoUrl
+        to: getGroupMembersIds()
+    }, requestCallback);
+}
 
