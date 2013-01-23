56b278f127e354726bce83be59bc029efd0f1ce7
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2013-01-22 23:13:02 -0200

    Envia notificação para o grupo ao cadastrar novo anúncio de plantão.

--- app/assets/javascripts/facebook-login.js
+++ app/assets/javascripts/facebook-login.js
@@ -1,15 +1,3 @@
-window.fbAsyncInit = function() {
-    FB.init({
-        appId      : '492391947466622', // App ID
-        channelUrl : '//queroplantao.com.br', // Channel File
-        status     : true, // check login status
-        cookie     : true, // enable cookies to allow the server to access the session
-        xfbml      : true,  // parse XFBML
-        frictionlessRequests : true
-    });
-    // Additional initialization code here
-};
-
 // Load the SDK Asynchronously
 (function(d){
     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
@@ -19,43 +7,57 @@ window.fbAsyncInit = function() {
     ref.parentNode.insertBefore(js, ref);
 }(document));
 
-
-function fb_login(){
-    var access_token;
-    FB.login(function(response) {
-        if (response.authResponse) {
-            access_token = response.authResponse.accessToken;
-            FB.api('/me', function(response) {
-                if(response.name != 'undefined'){
-                    id_profile = response.id;
-                    nome = response.name;
-                    email = response.email;
-                    avatar = "http://graph.facebook.com/"+id_profile+"/picture?type=square";
-                    // autenticaFB(id_profile, nome, avatar, email, access_token, origem);
-                }
-            });
-        }
-    },{scope: 'email,publish_stream, sms'});
-}
-
-
-// http://developers.facebook.com/docs/reference/api/group/
-function sendRequestToRecipients(plantaoUrl) {
-    //Grupo, PLANTÕES Whatsapp RJ: 132347300260952
-    //Grupo, Quero Plantao: 319924641441589
-    FB.api('319924641441589/members', {limit: 0}, function (response) {
-        var total = response.data.length;
-        var members = [];
-        for(var i=0; i < total; i++) {
-            var member = response.data[i];
-            members[members.length] = member.id;
-        }
-        FB.ui({method: 'apprequests',
-            message: 'Novo plantão adicionado.',
-            redirect_uri: plantaoUrl,
-            to: members
-        }, function (response) {
-            console.log(response);
+$(document).ready(function(){
+    function facebookReady(){
+        FB.init({
+            appId      : '492391947466622', // App ID
+            channelUrl : '//queroplantao.com.br', // Channel File
+            status     : true, // check login status
+            cookie     : true, // enable cookies to allow the server to access the session
+            xfbml      : true  // parse XFBML
+            //, frictionlessRequests : true //FIXME do not require user action
         });
-    });
+        $(document).trigger("facebook:ready");
+    }
+
+    if(window.FB) {
+        facebookReady();
+    } else {
+        window.fbAsyncInit = facebookReady;
+    }
+});
+
+var FACEBOOK = {
+    login: function () {
+        FB.login(function(response) {
+            if (response.authResponse) {
+                var access_token = response.authResponse.accessToken;
+                $(document).trigger("facebook:logged");
+            }
+        }, {scope: 'email, publish_stream, sms'});
+    },
+
+    // http://developers.facebook.com/docs/reference/api/group/
+    sendRequestToRecipients: function (plantaoUrl) {
+        //Grupo, PLANTÕES Whatsapp RJ: 132347300260952
+        //Grupo, Quero Plantao: 319924641441589
+        FB.api('319924641441589/members', {limit: 0}, function(response) {
+            if(response.error != undefined) {
+                alert('Algo deu errado');
+            } else {
+                var total = response.data.length;
+                var members = [];
+                for(var i=0; i < total; i++) {
+                    var member = response.data[i];
+                    members[members.length] = member.id;
+                }
+                FB.ui({method: 'apprequests',
+                    message: 'Novo plantão adicionado.',
+                    redirect_uri: plantaoUrl,
+                    to: members
+                }, function (response) {
+                });
+            }
+        });
+    }
 }
