520a9ad532ae7b68a66eb8eff80187bb3a1f656b
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2013-01-26 05:15:36 -0200

    Remove a informação de contato, quando usuário não estiver logado.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,11 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to root_url
+    redirect_to request.env['HTTP_REFERER']
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => "Signed out!"
+    redirect_to request.env['HTTP_REFERER'], :notice => "Signed out!"
   end
 end
