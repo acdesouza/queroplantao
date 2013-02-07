137acc91b668690d158f72890859e75feda57b3e
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2013-02-07 02:03:21 -0200

    Orderna por data do plantão mais próxima, maior remuneração, criado primeiro.

--- app/models/plantao.rb
+++ app/models/plantao.rb
@@ -1,5 +1,6 @@
 class Plantao < ActiveRecord::Base
   attr_accessible :contactName, :contactPhone, :how_long, :how_much, :obs, :specialty, :when, :where
 
-  scope :to_come, where('"plantoes"."when" > ?', Date.today)
+  scope :to_come, where('"plantoes"."when" > ?', Date.today).
+                  order('"plantoes"."when" ASC, "plantoes"."how_much" DESC, "plantoes"."created_at" ASC')
 end
