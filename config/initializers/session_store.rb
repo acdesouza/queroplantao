5236a6151c0b289ce731b99dd44801563d62a550
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2013-03-04 01:20:11 -0300

    Corrige a sessao para o nome do projeto.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-QueroPlantao::Application.config.session_store :cookie_store, key: '_pega-medico_session'
+QueroPlantao::Application.config.session_store :cookie_store, key: '_quero-plantao_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
