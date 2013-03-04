e21486d5698f5cc1a70b90125e294dad3802f2fb
zacaster <zacaster@gmail.com>
2013-03-04 09:08:26 -0300

    Adicionado o current_advertiser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,9 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+
+  def current_advertiser
+    @current_advertiser ||= Advertiser.find(session[:advertiser_id]) if session[:advertiser_id]
+  end
+  helper_method :current_advertiser
 end
