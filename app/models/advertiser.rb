c4d6050ea36ab0c31f14ce8059bbea4deb4d0565
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2013-03-05 02:36:07 -0300

    Impede de criar um Advertiser sem senha.

--- app/models/advertiser.rb
+++ app/models/advertiser.rb
@@ -1,6 +1,4 @@
 class Advertiser < ActiveRecord::Base
-  attr_accessible :email,:password, :password_digest
+  attr_accessible :email, :password, :password_digest
   has_secure_password
-  validates_presence_of :password, :on=>:create
- 
 end
