ab92f7e2ea0f9461d18ea090fdfd6fee68f6a644
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2013-03-14 02:05:51 -0300

    Tela de exibição para gerência de plantões.

--- app/controllers/advertiser_plantoes_controller.rb
+++ app/controllers/advertiser_plantoes_controller.rb
@@ -1,13 +1,11 @@
 #-*- coding: utf-8 -*-
 class AdvertiserPlantoesController < ApplicationController
   def index
-    plantoes = Advertiser.find(params[:advertiser_id]).plantoes
-
-    @plantoes_grouped = group_by_when_and_how_long(plantoes)
+    @plantoes = Advertiser.find(params[:advertiser_id]).plantoes
 
     respond_to do |format|
       format.html
-      format.json { render json: @plantoes_grouped }
+      format.json { render json: @plantoes }
     end
   end
 end
