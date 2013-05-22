f9fb60d127ab8c0f4a61ab4cd618afe7dcb4a9ea
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2013-05-22 12:02:02 -0300

    Separa o css da landpage dos outros.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,8 @@ QueroPlantao::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( application-pages.css )
+  #config.assets.precompile += %w( pages/pages.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
