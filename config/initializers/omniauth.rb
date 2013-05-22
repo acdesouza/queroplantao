22ca547b380fe4a20c1987fb4864f683f3bbf8ed
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2013-05-22 17:24:04 -0300

    Extrai a API Key e Secret, do Facebook, para uma variável de ambiente.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,8 +2,8 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-  FB_KEY    = '492391947466622'
-  FB_SECRET = '39d5f5c47777c18462944d3c5abe004b'
+  FB_KEY    = ENV['FACEBOOK_KEY']
+  FB_SECRET = ENV['FACEBOOK_SECRET']
   provider :facebook, FB_KEY, FB_SECRET
   #:scope => 'email,user_location', :display => 'popup'
 end
