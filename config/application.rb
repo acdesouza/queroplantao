f3f2d193cdde36120f7fa59db415a621673e1f84
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2013-05-24 15:38:01 -0300

    Assets config solicitada pelo Heroku para o Rails 3.x

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module QueroPlantao
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Heroku: https://devcenter.heroku.com/articles/rails-asset-pipeline#troubleshooting
+    config.assets.initialize_on_precompile = false
   end
 end
