46953adfde2783a52a078a6e66a0e41f5a482832
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2013-05-29 19:09:30 -0300

    Após o login, leva para a tela de edição do User, se não tiver telefone, ou a página de agradecimento, se tiver.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,13 @@ class PagesController < ApplicationController
   skip_before_filter :authenticate_user, only: [:land]
 
   def land
+    unless current_user.nil?
+      if current_user.phone.nil?
+        redirect_to edit_user_path
+      else
+        redirect_to greeting_path
+      end
+    end
   end
 
   def greeting
--- test/functional/pages_controller_test.rb
+++ test/functional/pages_controller_test.rb
@@ -6,6 +6,20 @@ class PagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "landpage should redirect to user#edit when user hasn't a phone" do
+    sign_in users(:ac)
+
+    get :land
+    assert_redirected_to edit_user_path
+  end
+
+  test "landpage should redirect to pages#greeting when user has a phone" do
+    sign_in users(:marina)
+
+    get :land
+    assert_redirected_to greeting_path
+  end
+
   test "should get greeting" do
     sign_in users(:ac)
 
